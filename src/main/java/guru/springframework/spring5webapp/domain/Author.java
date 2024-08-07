b9fe9402d4c6b98a452c73f4601d14c9f68c4a93
Super Thaddeus <supertha@student.21-school.ru>
2024-08-07 14:33:37 +0700

    Parts 15, 16 done. Author.java, Book.java added to domain package. AuthorRepository.java, BookRepository.java added to repositories package

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 @Entity
 public class Author {
@@ -53,5 +54,30 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
